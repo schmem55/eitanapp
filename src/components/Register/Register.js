01cf2e460a65f7c5d35bbcba3d7a5d31a52bd022
schmem55 <scemama.jeremy@gmail.com>
2020-07-22 15:58:16 +0300

    icons added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import './Register.css';
+import {FaFacebookF} from 'react-icons/fa'
+import {FaWhatsapp} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaLinkedinIn} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+
 export default function Register() {
   return (
     <div className="Register">
-      <h3>Connect to your account</h3>
+      <h3 className='Title'>Connect to your account</h3>
+      <div className="Apps">
+        <button  className="IconApp">
+          <FaFacebookF size={30}/>  
+        </button>
+        <button  className="IconApp">
+          <FaWhatsapp size={30}/> 
+        </button>
+        <button  className="IconApp">
+          <FaInstagram  size={30}/>  
+        </button>
+        <button  className="IconApp">
+          <FaLinkedinIn  size={30}/>  
+        </button>
+        <button  className="IconApp">
+          <FaTwitter size={30}/>  
+        </button>
+        <button  className="IconApp">
+          <img width="30" height="30" src={require('../../images/tiktok.png')}/>
+        </button>
+
+        
+        
+        
+    
+      </div>
     </div>
   );
 }
